272c9f181d95910385ad24f8dfcbf678f0bd48fa
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-16 14:35:54 +0100

    Fix row

---
+++ Milestone 3/js/script.js
@@ -0,0 +1,28 @@
+// CONSEGNA
+// Scrivi un programma che stampi i numeri da 1 a 100.
+
+
+// Milestone 2:
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+
+for(let i = 1; i <= 1000; i++){
+    let numberOrWord;
+    
+    if(i % 3 === 0 && i % 5 === 0){
+        numberOrWord = `FizzBuzz`;
+    } else if(i % 3 === 0){
+        numberOrWord = `Fizz`;
+    } else if(i % 5 === 0){
+        numberOrWord = `Buzz`;
+    }  else{
+        numberOrWord = i;
+    }
+
+    // console.log(numberOrWord);
+
+    let mainRow = document.querySelector('.row');
+    mainRow.innerHTML += `<div class="col_7"><div class="d-flex justify-content-center align-items-center p-5 fw-bolder">${numberOrWord}</div></div>`;
+
+}
+
+
