e1fe0d493f90c49e9bb330b39d764545e5b4f166
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-16 14:56:45 +0100

    Finita Milestone 3

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -2,26 +2,30 @@
 // Scrivi un programma che stampi i numeri da 1 a 100.
 
 
-// Milestone 2:
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+// Milestone 3:
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
 for(let i = 1; i <= 1000; i++){
     let numberOrWord;
-    
+    let backgroundClass;
     if(i % 3 === 0 && i % 5 === 0){
         numberOrWord = `FizzBuzz`;
+        backgroundClass = `fizzbuzz_bg`;
     } else if(i % 3 === 0){
         numberOrWord = `Fizz`;
+        backgroundClass = `fizz_bg`;
     } else if(i % 5 === 0){
         numberOrWord = `Buzz`;
+        backgroundClass = `buzz_bg`;
     }  else{
         numberOrWord = i;
+        backgroundClass = `number_bg`;
     }
 
     // console.log(numberOrWord);
 
     let mainRow = document.querySelector('.row');
-    mainRow.innerHTML += `<div class="col"><div class="d-flex justify-content-center align-items-center p-5 fw-bolder">${numberOrWord}</div></div>`;
+    mainRow.innerHTML += `<div class="col"><div class="d-flex justify-content-center align-items-center p-5 fw-bolder ${backgroundClass}">${numberOrWord}</div></div>`;
 
 }
 
